075c00fa532c681b6532585023d39045794dfcab
Henry-Azer <henryazer@outlook.com>
2023-03-02 16:39:09 +0200

    integrate react-drawing-board package

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import DrawingBoard from "react-drawing-board";
 
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
     const { t } = useTranslation();
 
-    return <div>{t("home:title")}</div>;
+    return (
+        <div>
+            <h1>{t("home:title")}</h1>
+            <DrawingBoard />
+        </div>
+    );
 };
 
 export default Home;
