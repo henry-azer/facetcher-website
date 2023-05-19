ad5b87ad598c926f56963d9af17a6846bd19f063
unknown <a_bavly@yahoo.com>
2023-05-19 17:45:10 +0200

    Update Select component

--- src/components/select-component.jsx
+++ src/components/select-component.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const FacetcherSelectComponent = (props) => {
      return (
           <div className={`w-${props.width}`}>
-               {props.label && <h1 className=" text-grey fs-6">{props.label}</h1>}
+               {props.label && <h1 className="fs-6">{props.label}</h1>}
                <select
+                    name={props.name && props.name}
+                    value={props.value && props.value}
                     defaultValue={props.defaultValue && props.defaultValue}
-                    className="bg-transparent grey-border fs-6 rounded-pill text-light-grey w-100 p-3"
+                    onChange={props.onChange}
+                    className="bg-transparent light-grey-border p-1 rounded-pill text-light-grey w-100"
                >
                     {props.options.map((option, index) => (
                          <option
